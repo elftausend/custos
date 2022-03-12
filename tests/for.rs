f8df6de1b298770fff9d56bd8d5a7e3bad58e3c2
elftausend <skatsacc.gut24@gmail.com>
2022-03-12 14:36:14 +0100

    added inter

--- tests/for.rs
+++ tests/for.rs
@@ -1,8 +1,20 @@
 
 struct Count(usize, usize);
 
+struct CountIntoIter(usize);
+
+impl IntoIterator for Count {
+    type Item = usize;
+
+    type IntoIter = CountIntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        todo!()
+    }
+}
 
 #[test]
 fn counting() {
+    for i in Count(0, 100) {}
 
 }
