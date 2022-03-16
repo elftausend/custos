751495c0b5dcd980cd861c9294f1a61863db800b
elftausend <skatsacc.gut24@gmail.com>
2022-03-16 22:38:00 +0100

    updated test

--- tests/get_device.rs
+++ tests/get_device.rs
@@ -1,4 +1,4 @@
-use custos::{Matrix, get_device, VecRead, libs::{cpu::CPU, opencl::CLDevice}, AsDev, GLOBAL_DEVICE};
+use custos::{Matrix, get_device, VecRead, libs::{cpu::CPU, opencl::CLDevice}, AsDev, GLOBAL_DEVICE, BaseDevice};
 
 #[test]
 fn test_matrix_read() {
@@ -17,5 +17,8 @@ fn test_matrix_read() {
     let matrix = Matrix::from(((2, 3), &[1.51, 6.123, 7., 5.21, 8.62, 4.765]));
     let read = read.read(matrix.data());
     assert_eq!(&read, &[1.51, 6.123, 7., 5.21, 8.62, 4.765]);
+    
+    let base_device = get_device!(BaseDevice, f32);
+    assert_eq!(&read, &base_device.read(matrix.data()));
     
 }
