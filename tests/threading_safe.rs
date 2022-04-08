52f314ef5dc257719bf59d2d25d9a42bd25c0759
elftausend <skatsacc.gut24@gmail.com>
2022-04-08 21:56:56 +0200

    added

--- tests/threading_safe.rs
+++ tests/threading_safe.rs
@@ -1,11 +1,11 @@
-#[cfg(not(feature="safe"))]
+#[cfg(feature="safe")]
 use custos::{libs::cpu::CPU, Matrix, AsDev, range, VecRead};
 
-#[cfg(not(feature="safe"))]
+#[cfg(feature="safe")]
 #[cfg(feature="opencl")]
 use custos::libs::opencl::CLDevice;
 
-#[cfg(not(feature="safe"))]
+#[cfg(feature="safe")]
 #[test]
 fn test_threading_cpu() {
     let device = CPU::new().select();
@@ -18,25 +18,25 @@ fn test_threading_cpu() {
         
         for _ in range(500) {
             
-            let c = a * b;
+            let c = &a * &b;
             assert_eq!(device.read(c.data()), vec![3., 6., 2., 30., 30., 16.]);
 
         }
-        CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 1));
+        //CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 1));
 
         for _ in range(500) {
-            let c = a - b;
-            let d = a + b + c;
-            let e = a * b - c + d * d - a;
+            let c = &a - &b;
+            let d = &a + &b + &c;
+            let e = &a * &b - &c + &d * &d - &a;
             assert_eq!(34., e.read()[0]);    
         }
-        CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
+       // CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
 
-        let c = a - b;
-        let d = a + b + c;
-        let e = a * b - c + d * d - a;
+        let c = &a - &b;
+        let d = &a + &b + &c;
+        let e = &a * &b - &c + &d * &d - &a;
         assert_eq!(34., e.read()[0]);
-        CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
+       // CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
     });
 
 
@@ -48,24 +48,24 @@ fn test_threading_cpu() {
         
         for _ in range(500) {
             
-            let c = a * b;
+            let c = &a * &b;
             assert_eq!(device.read(c.data()), vec![3., 6., 2., 30., 30., 16.]);
         }
-        CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 1));
+     //   CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 1));
 
         for _ in range(500) {
-            let c = a - b;
-            let d = a + b + c;
-            let e = a * b - c + d * d - a;
+            let c = &a - &b;
+            let d = &a + &b + &c;
+            let e = &a * &b - &c + &d * &d - &a;
             assert_eq!(34., e.read()[0]);    
         }
-        CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
+        //CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
 
-        let c = a - b;
-        let d = a + b + c;
-        let e = a * b - c + d * d - a;
+        let c = &a - &b;
+        let d = &a + &b + &c;
+        let e = &a * &b - &c + &d * &d - &a;
         assert_eq!(34., e.read()[0]);
-        CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
+      //  CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
     });
 
 
@@ -78,20 +78,20 @@ fn test_threading_cpu() {
             
             for _ in range(500) {
                 
-                let c = a + b;
+                let c = &a + &b;
                 assert_eq!(device.read(c.data()), vec![4., 5., 3., 11., 11., 8.]);
 
 
                 for _ in range(5) {
-                    let d = a * b * c;
-                    let _ = d + c - (b + a * d);
+                    let d = &a * &b * &c;
+                    let _ = &d + &c - ( &b + &a * &d);
                     
                 }
-                CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 7));
+      //          CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 7));
             }
         } //'device' is dropped
         
-        CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 0));
+      //  CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 0));
 
     });
 
@@ -100,20 +100,18 @@ fn test_threading_cpu() {
     
     for _ in range(500) {
         
-        let c = a - b;
+        let c = &a - &b;
         assert_eq!(c.read(), vec![2., -1., -1., -1., 1., 0.]);
     }
 
-    CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 1));
-
-use custos::cpu::CPU_CACHE;
+   // CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 1));
 
     th1_cl.join().unwrap();
     th1_cpu.join().unwrap();
     th2.join().unwrap();
 }
 
-#[cfg(not(feature="safe"))]
+#[cfg(feature="safe")]
 #[cfg(feature="opencl")]
 #[test]
 fn test_threading_cl_a() {
@@ -127,25 +125,25 @@ fn test_threading_cl_a() {
         
         for _ in range(500) {
             
-            let c = a * b;
+            let c = &a * &b;
             assert_eq!(device.read(c.data()), vec![3., 6., 2., 30., 30., 16.]);
 
         }
-        CL_CACHE.with(|f| assert!(f.borrow().output_nodes.len() == 1));
+  //      CL_CACHE.with(|f| assert!(f.borrow().output_nodes.len() == 1));
 
         for _ in range(500) {
-            let c = a - b;
-            let d = a + b + c;
-            let e = a * b - c + d * d - a;
+            let c = &a - &b;
+            let d = &a + &b + &c;
+            let e = &a * &b - c + &d * &d - &a;
             assert_eq!(34., e.read()[0]);    
         }
-        CL_CACHE.with(|f| assert!(f.borrow().output_nodes.len() == 8));
+//        CL_CACHE.with(|f| assert!(f.borrow().output_nodes.len() == 8));
 
-        let c = a - b;
-        let d = a + b + c;
-        let e = a * b - c + d * d - a;
+        let c = &a - &b;
+        let d = &a + &b + &c;
+        let e = &a * &b - &c + &d * &d - &a;
         assert_eq!(34., e.read()[0]);
-        CL_CACHE.with(|f| assert!(f.borrow().output_nodes.len() == 8));
+   //     CL_CACHE.with(|f| assert!(f.borrow().output_nodes.len() == 8));
     });
 
 
@@ -157,24 +155,24 @@ fn test_threading_cl_a() {
         
         for _ in range(500) {
             
-            let c = a * b;
+            let c = &a * &b;
             assert_eq!(device.read(c.data()), vec![3., 6., 2., 30., 30., 16.]);
         }
-        CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 1));
+     //   CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 1));
 
         for _ in range(500) {
-            let c = a - b;
-            let d = a + b + c;
-            let e = a * b - c + d * d - a;
+            let c = &a - &b;
+            let d = &a + &b + &c;
+            let e = &a * &b - &c + &d * &d - &a;
             assert_eq!(34., e.read()[0]);    
         }
-        CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
+   //     CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
 
         let c = &a - &b;
         let d = &a + &b + &c;
         let e = &a * &b - &c + &d * &d - &a;
         assert_eq!(34., e.read()[0]);
-        CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
+   //     CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 8));
     });
 
 
@@ -187,20 +185,20 @@ fn test_threading_cl_a() {
             
             for _ in range(500) {
                 
-                let c = a + b;
+                let c = &a + &b;
                 assert_eq!(device.read(c.data()), vec![4., 5., 3., 11., 11., 8.]);
 
 
                 for _ in range(5) {
-                    let d = a * b * c;
-                    let _ = d + c - (b + a * d);
+                    let d = &a * &b * &c;
+                    let _ = &d + &c - (&b + &a * &d);
                     
                 }
-                CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 7));
+  //              CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 7));
             }
         } //'device' is dropped
         
-        CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 0));
+   //     CPU_CACHE.with(|f| assert!(f.borrow().nodes.len() == 0));
 
     });
 
@@ -209,13 +207,11 @@ fn test_threading_cl_a() {
     
     for _ in range(500) {
         
-        let c = a - b;
+        let c = &a - &b;
         assert_eq!(c.read(), vec![2., -1., -1., -1., 1., 0.]);
     }
 
    // CL_CACHE.with(|f| assert!(f.borrow().output_nodes.len() == 1));
-
-use custos::{opencl::CL_CACHE, cpu::CPU_CACHE};
 
     th1_cl.join().unwrap();
     th1_cpu.join().unwrap();
