3249c3e4e20051660389cb52ed554dbc501c9664
elftausend <skatsacc.gut24@gmail.com>
2022-05-04 21:00:55 +0200

    examples

---
+++ examples/gemm.rs
@@ -0,0 +1,13 @@
+use custos::{CPU, AsDev, Matrix};
+
+fn main() {
+    let device = CPU::new().select();
+
+    let a = Matrix::from((&device, (2, 3), [1., 2., 3., 4., 5., 6.,]));
+    let b = Matrix::from((&device, (3, 2), [6., 5., 4., 3., 2., 1.,]));
+
+    let c = a.gemm(&b);
+    println!("c: {c:?}");
+
+    assert_eq!(c.read(), vec![20., 14., 56., 41.,]);
+}
