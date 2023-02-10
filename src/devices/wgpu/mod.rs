3f243d7880d7ab1f8a20fef1168109c8079229f8
elftausend <76885970+elftausend@users.noreply.github.com>
2023-02-10 10:55:11 +0100

    Add wgpu buffer clear

--- src/devices/wgpu/mod.rs
+++ src/devices/wgpu/mod.rs
@@ -10,3 +10,53 @@ pub use wgpu_device::*;
 
 use crate::{Buffer, Shape};
 
+/// Sets all the elements of a `WGPU` `Buffer` to zero / default.
+/// 
+/// # Example
+/// ```
+/// use crate::{WGPU, Buffer, wgpu::wgpu_clear};
+/// 
+/// fn main() -> custos::Result<()> {
+///     let device = WGPU::new(wgpu::Backends::all())?;
+///     let mut buf = Buffer::from((&device, [4f32, -1.2, 2., 1., 3.]));
+///     wgpu_clear(&device, &mut buf);
+///     
+///     assert_eq!(buf.read(), [0., 0., 0., 0., 0.,]);
+///     Ok(())
+/// }
+/// ```
+pub fn wgpu_clear<T: Default + Debug, S: Shape>(device: &WGPU, buf: &mut Buffer<T, WGPU, S>) {
+    let src = format!(
+        "@group(0)
+        @binding(0)
+        var<storage, read_write> buf: array<{datatype}>;
+        
+        @compute
+        @workgroup_size(1)
+        fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {{
+            buf[global_id.x] = {zero:?};
+        }}
+        ",
+        datatype = std::any::type_name::<T>(),
+        zero = T::default()
+    );
+
+    launch_shader(device, &src, [buf.len() as u32, 1, 1], &[buf])
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{WGPU, Buffer};
+
+    #[test]
+    fn test_wgpu_clear() -> crate::Result<() >{
+        let device = WGPU::new(wgpu::Backends::all())?;
+
+        let mut buf = Buffer::from((&device, [1, 4, 2, 1, 9]));
+        buf.clear();
+
+        assert_eq!(buf.read(), [0, 0, 0, 0, 0]);
+
+        Ok(())
+    }
+}
