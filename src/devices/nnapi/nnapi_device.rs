8e90fb0a96cf266451a7a5bd945d90eba615359f
elftausend <76885970+elftausend@users.noreply.github.com>
2023-05-03 10:26:49 +0200

    Reuse nnapi compilation for every following run

--- src/devices/nnapi/nnapi_device.rs
+++ src/devices/nnapi/nnapi_device.rs
@@ -4,7 +4,7 @@ use core::{
     ffi::c_void,
     mem::{ManuallyDrop, size_of},
 };
-use nnapi::{AsOperandCode, Model, Operand};
+use nnapi::{AsOperandCode, Model, Operand, Compilation};
 
 
 type ArrayId = (u32, ArrayPtr);
@@ -12,8 +12,9 @@ type ArrayId = (u32, ArrayPtr);
 pub struct NnapiDevice {
     pub model: RefCell<Model>,
     operand_count: Cell<u32>,
-    input_ptrs: RefCell<Vec<ArrayId>>,
+    pub input_ptrs: RefCell<Vec<ArrayId>>,
     // last_output: RefCell<Option<NnapiPtr>>,
+    compilation: RefCell<Option<Compilation>>,
     addons: Addons<Self>,
 }
 
@@ -31,11 +32,12 @@ impl NnapiDevice {
             operand_count: Cell::new(0),
             addons: Addons::default(),
             input_ptrs: Default::default(),
+            compilation: Default::default(),
             // last_output: Default::default(),
         })
     }
 
-    pub fn run<T: Default + Copy + AsOperandCode, S: Shape>(&self, out: Buffer<T, Self, S>) -> crate::Result<Vec<T>> {
+    pub fn compile<T, S: Shape>(&self, out: Buffer<T, Self, S>) -> crate::Result<()> {
         let mut model = self.model.borrow_mut();
 
         let input_ids = self.input_ptrs.borrow().iter().map(|(id, _)| *id).collect::<Vec<u32>>();
@@ -44,8 +46,20 @@ impl NnapiDevice {
 
         model.identify_inputs_and_outputs(&input_ids, &[out.ptr.idx])?;
 
+        model.finish()?;
         let mut compilation = model.compile()?;
         compilation.finish()?;
+        *self.compilation.borrow_mut() = Some(compilation);
+        Ok(())
+    }
+
+    pub fn run<T, S>(&self, out: Buffer<T, Self, S>) -> crate::Result<Vec<T>> where T: Default + Copy + AsOperandCode, S: Shape {
+        if self.compilation.borrow().is_none() {
+            self.compile(out)?;
+        }
+        let mut compilation = self.compilation.borrow_mut();
+        let compilation = compilation.as_mut().unwrap();
+
         let mut run = compilation.create_execution()?;
 
         for (idx, (_id, input_ptr)) in self.input_ptrs.borrow().iter().enumerate() {
@@ -180,23 +194,41 @@ mod tests {
 
     #[test]
     fn test_nnapi_device() -> crate::Result<()> {
-        let device = super::NnapiDevice::new()?;
+        let mut device = super::NnapiDevice::new()?;
 
         let lhs = Buffer::with(&device, &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         let rhs = Buffer::with(&device, &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
-        let mut out = device.retrieve::<i32, Dim1<10>>(lhs.len(), ());
-
-        {
+        
+        // a single operation
+        let out = {
+            let out = device.retrieve::<i32, Dim1<10>>(lhs.len(), ());
             let activation_idx = device.add_operand(Operand::activation())?;
             let mut model = device.model.borrow_mut();
+
             model.set_activation_operand_value(activation_idx as i32)?;
             model.add_operation(
                 OperationCode::ANEURALNETWORKS_ADD,
                 &[lhs.ptr.idx, rhs.ptr.idx, activation_idx],
                 &[out.ptr.idx],
             )?;
-        }
+            out
+        };
+
+        // another one
+        /*let out = {
+            let out1 = device.retrieve::<i32, Dim1<10>>(lhs.len(), ());
+            let activation_idx = device.add_operand(Operand::activation())?;
+            let mut model = device.model.borrow_mut();
+
+            model.set_activation_operand_value(activation_idx as i32)?;
+            model.add_operation(
+                OperationCode::ANEURALNETWORKS_ADD,
+                &[out.ptr.idx, rhs.ptr.idx, activation_idx],
+                &[out1.ptr.idx],
+            )?;
+            out1
+        };*/
 
         device.run(out)?;   
 
