7236085ce463001bcef62f10efbfc7f7da8a6c43
elftausend <76885970+elftausend@users.noreply.github.com>
2023-07-31 21:27:50 +0200

    Remove AutogradModule code

--- src/module_comb/modules/autograd.rs
+++ src/module_comb/modules/autograd.rs
@@ -92,49 +92,6 @@ impl<Mods: Retrieve<D>, D> Retrieve<D> for Autograd<Mods> {
     }
 }
 
-/*
-impl<Mods: Module<SD>, SD: Alloc> Module<SD> for Autograd<Mods> {
-    type Module = AutogradModule<Mods::Module, SD>;
-
-    #[inline]
-    fn new() -> Self::Module {
-        AutogradModule {
-            modules: Mods::new(),
-            pd: PhantomData,
-        }
-    }
-}
-
-// remove
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
-pub struct AutogradModule<Mods, D: Alloc> {
-    modules: Mods,
-    pd: PhantomData<D>,
-}
-
-impl<Mods: Setup<NewDev>, D: Alloc, NewDev> Setup<NewDev> for AutogradModule<Mods, D> {
-    #[inline]
-    fn setup(device: &mut NewDev) {
-        Mods::setup(device)
-    }
-}
-
-impl<Mods: OnDropBuffer, SD: Alloc> OnDropBuffer for AutogradModule<Mods, SD> {
-    fn on_drop<'a, T, D: Device, S: Shape>(&self, device: &'a D, buf: &Buffer<T, D, S>) {
-        self.modules.on_drop(device, buf)
-    }
-}
-
-impl<Mods: Retrieve<D>, D, SimpleDevice: Alloc> Retrieve<D> for AutogradModule<Mods, SimpleDevice> {
-    #[inline]
-    fn retrieve<T, S: crate::Shape>(&self, device: &D, len: usize) -> <D>::Data<T, S>
-    where
-        D: Alloc,
-    {
-        self.modules.retrieve(device, len)
-    }
-}
-*/
 #[cfg(test)]
 mod tests {
     use core::any::Any;
