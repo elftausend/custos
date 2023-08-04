a90666d35d95d9141ba29abc7da730037663aaef
elftausend <76885970+elftausend@users.noreply.github.com>
2023-08-04 22:38:24 +0200

    Add cpu ops and impl WriteBuf for new CPU

---
+++ src/module_comb/devices/cpu/ops.rs
@@ -0,0 +1,15 @@
+use crate::{module_comb::{MainMemory, WriteBuf, Buffer, OnDropBuffer}, Shape};
+
+use super::CPU;
+
+impl<Mods: OnDropBuffer, T: Copy, D: MainMemory, S: Shape> WriteBuf<T, S, D> for CPU<Mods> {
+    #[inline]
+    fn write(&self, buf: &mut Buffer<T, D, S>, data: &[T]) {
+        buf.copy_from_slice(data)
+    }
+
+    #[inline]
+    fn write_buf(&self, dst: &mut Buffer<T, D, S>, src: &Buffer<T, D, S>) {
+        self.write(dst, src)
+    }
+}
