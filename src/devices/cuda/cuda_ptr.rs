d75570c01c6dd7a83dbaa4276a1ab937456b8f8d
elftausend <76885970+elftausend@users.noreply.github.com>
2023-09-16 15:54:31 +0200

    Move CUDAPtr to a separate file

---
+++ src/devices/cuda/cuda_ptr.rs
@@ -0,0 +1,90 @@
+use core::{marker::PhantomData, ptr::null_mut};
+
+use crate::{flag::AllocFlag, HasId, Id, ShallowCopy, CommonPtrs, PtrType};
+
+use super::api::cufree;
+
+/// The pointer used for `CUDA` [`Buffer`](crate::Buffer)s
+#[derive(Debug, PartialEq, Eq)]
+pub struct CUDAPtr<T> {
+    /// The pointer to the CUDA memory object.
+    pub ptr: u64,
+    /// The number of elements addressable
+    pub len: usize,
+    /// Allocation flag for the pointer.
+    pub flag: AllocFlag,
+    pub p: PhantomData<T>,
+}
+
+impl<T> HasId for CUDAPtr<T> {
+    #[inline]
+    fn id(&self) -> Id {
+        Id {
+            id: self.ptr,
+            len: self.len,
+        }
+    }
+}
+
+impl<T> Default for CUDAPtr<T> {
+    #[inline]
+    fn default() -> Self {
+        Self {
+            ptr: 0,
+            len: 0,
+            flag: AllocFlag::default(),
+            p: PhantomData,
+        }
+    }
+}
+
+impl<T> Drop for CUDAPtr<T> {
+    fn drop(&mut self) {
+        if !matches!(self.flag, AllocFlag::None | AllocFlag::BorrowedCache) {
+            return;
+        }
+
+        if self.ptr == 0 {
+            return;
+        }
+        unsafe {
+            cufree(self.ptr).unwrap();
+        }
+    }
+}
+
+impl<T> ShallowCopy for CUDAPtr<T> {
+    #[inline]
+    unsafe fn shallow(&self) -> Self {
+        CUDAPtr {
+            ptr: self.ptr,
+            len: self.len,
+            flag: AllocFlag::Wrapper,
+            p: PhantomData,
+        }
+    }
+}
+
+impl<T> PtrType for CUDAPtr<T> {
+    #[inline]
+    fn size(&self) -> usize {
+        self.len
+    }
+
+    #[inline]
+    fn flag(&self) -> AllocFlag {
+        self.flag
+    }
+}
+
+impl<T> CommonPtrs<T> for CUDAPtr<T> {
+    #[inline]
+    fn ptrs(&self) -> (*const T, *mut std::ffi::c_void, u64) {
+        (null_mut(), null_mut(), self.ptr)
+    }
+
+    #[inline]
+    fn ptrs_mut(&mut self) -> (*mut T, *mut std::ffi::c_void, u64) {
+        (null_mut(), null_mut(), self.ptr)
+    }
+}
