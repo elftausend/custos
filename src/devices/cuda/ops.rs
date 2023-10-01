18665d900db243d60eb61346e6ba93cf4addc9e5
elftausend <76885970+elftausend@users.noreply.github.com>
2023-10-01 19:17:14 +0200

    Add cu apply fn and cu add unary grad tests

--- src/devices/cuda/ops.rs
+++ src/devices/cuda/ops.rs
@@ -4,7 +4,7 @@ use crate::{
     bounds_to_range,
     cuda::api::{cu_read_async, CUstreamCaptureStatus},
     pass_down_add_operation, ApplyFunction, Buffer, CDatatype, ClearBuf, CopySlice, OnDropBuffer,
-    Read, Resolve, Retrieve, Retriever, Shape, ToCLSource, ToMarker, WriteBuf, CUDA,
+    Read, Resolve, Retrieve, Retriever, Shape, ToCLSource, ToMarker, WriteBuf, CUDA, AddOperation, UnaryGrad,
 };
 
 use super::{
@@ -159,7 +159,7 @@ where
 
 impl<T, S, Mods: OnDropBuffer + AddOperation<T, Self>> UnaryGrad<T, S> for CUDA<Mods>
 where
-    T: CDatatype + Number,
+    T: CDatatype + Default,
     S: Shape,
 {
     #[inline]
@@ -186,23 +186,55 @@ pub fn try_cu_add_unary_grad<T, F, Mods: OnDropBuffer>(
     lhs_grad_fn: impl Fn(Resolve<T>) -> F,
 ) -> crate::Result<()>
 where
-    T: CDatatype + Number,
+    T: CDatatype + Default,
     F: ToCLSource,
-{
+{ 
     let src = format!(
-        "
-        extern "C" __global__ void addUnaryGrad({datatype}* lhs, {datatype}* lhsGrad, {datatype}* out, int numElements)
+        r#"
+        extern "C" __global__ void addUnaryGrad({dtype}* lhs, {dtype}* lhsGrad, {dtype}* out, int numElements)
             {{
                 int idx = blockDim.x * blockIdx.x + threadIdx.x;
                 if (idx >= numElements) {{
                     return;
                 }}
-                lhs_grad[idx] += out[idx] * {op};
+                lhsGrad[idx] += out[idx] * {op};
             }}
-    ",
-        dtype = std::any::type_name::<T>(),
+    "#,
+        dtype = T::C_DTYPE_STR,
         op = lhs_grad_fn("lhs[idx]".to_marker()).to_cl_source()
     );
-    device.launch_kernel1d(buf.len, &src, "addUnaryGrad", &[lhs, lhs_grad, out, &lhs.len])?;
+    device.launch_kernel1d(lhs.len, &src, "addUnaryGrad", &[lhs, lhs_grad, out, &lhs.len])?;
+    
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{CUDA, Base, Buffer, cuda::ops::{try_cu_apply_fn_mut, try_cu_add_unary_grad}, Combiner};
+
+    #[test]
+    fn test_cu_apply_fn() {
+        let device = CUDA::<Base>::new(0).unwrap();
+        let x = Buffer::from((&device, [1f32, 2., 3., 4., 5., 6.]));
+        let mut out = x.empty_like();
+        try_cu_apply_fn_mut(&device, &x.data, &mut out.data, |x| x.add("1.0")).unwrap();
+        assert_eq!(out.read(), [2f32, 3., 4., 5., 6., 7.,])
+    }
+    
+    #[test]
+    fn test_cu_add_unary_grad() -> crate::Result<()> {
+        let device = CUDA::<Base>::new(0)?;
+        let lhs = Buffer::from((&device, [1, 2, 3, 4, 5, 6]));
+        let mut lhs_grad = Buffer::from((&device, [1, 2, 3, 4, 5, 6]));
+
+        let out = Buffer::from((&device, [1, 1, 1, 1, 1, 1]));
+
+        try_cu_add_unary_grad(&device, &lhs.data, &mut lhs_grad.data, &out.data, |x| {
+            x.mul(2).add(1)
+        })?;
+
+        assert_eq!(lhs_grad.read(), [4, 7, 10, 13, 16, 19]);
+
+        Ok(())
+    }
 }
