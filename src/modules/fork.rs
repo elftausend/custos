f259ec200fb9aff9d921a8b15868a8f20b2afad7
elftausend <76885970+elftausend@users.noreply.github.com>
2024-01-06 14:29:30 +0100

    Move serde specifics of fork to serde.rs

--- src/modules/fork.rs
+++ src/modules/fork.rs
@@ -9,14 +9,12 @@ use std::collections::{BinaryHeap, HashMap};
 mod analyzation;
 mod fork_macro;
 mod use_gpu_or_cpu;
+mod serde;
 
 pub use analyzation::Analyzation;
 pub use use_gpu_or_cpu::*;
 
-
-#[cfg_attr(feature = "serde", derive(serde::Serialize))]
 pub struct Fork<Mods> {
-    #[cfg_attr(feature = "serde", serde(skip))]
     pub modules: Mods,
     pub gpu_or_cpu: RefCell<
         HashMap<HashLocation<'static>, BinaryHeap<Analyzation>, BuildHasherDefault<LocationHasher>>,
@@ -53,37 +51,6 @@ impl<Mods: Module<D>, D: Device> Module<D> for Fork<Mods> {
             modules: Mods::new(),
             gpu_or_cpu: Default::default(),
         }
-    }
-}
-
-#[cfg(all(feature = "serde", not(feature = "no-std")))]
-impl<Mods> Fork<Mods> {
-    #[inline]
-    pub fn load_from_deserializer<D: serde::Deserializer<'static>>(&mut self, deserializer: D) -> Result<(), D::Error> {
-        use serde::Deserialize;
-
-        self.gpu_or_cpu = RefCell::new(HashMap::deserialize(deserializer)?);
-        Ok(())
-    }
-
-    #[cfg(feature = "json")]
-    #[inline]
-    pub fn save_as_json(&self, path: impl AsRef<std::path::Path>) -> crate::Result<()> {
-        serde_json::to_writer(std::fs::File::open(path)?, self)?;
-        Ok(())
-    }
-
-    #[cfg(feature = "json")]
-    #[inline]
-    pub fn load_from_json_read(&mut self, reader: impl std::io::Read) -> serde_json::Result<()> {
-        self.load_from_deserializer(&mut serde_json::Deserializer::from_reader(reader))
-    }
-    
-    #[cfg(feature = "json")]
-    #[inline]
-    pub fn load_from_json(&mut self, path: impl AsRef<std::path::Path>) -> crate::Result<()> {
-        self.load_from_json_read(std::fs::File::open(path)?)?;
-        Ok(())
     }
 }
 
@@ -429,36 +396,4 @@ mod tests {
             assert_eq!(&analyzations[0].input_lengths, &[6]);
         }
     }
-
-    #[cfg(feature = "json")]
-    #[cfg(feature = "serde")]
-    #[cfg(feature = "cpu")]
-    #[test]
-    fn test_fork_deserialize() {
-        use serde::Serialize;
-
-        let device = OpenCL::<Fork<Cached<Base>>>::new(0).unwrap();
-        if !device.unified_mem() {
-            return;
-        }
-        let buf = device.buffer([1, 2, 4, 5, 6, 7]);
-        let out = device.apply_fn(&buf, |x| x.add(3));
-        assert_eq!(out.read(), [4, 5, 7, 8, 9, 10]);
-
-        for _ in 0..100 {
-            let _out = device.apply_fn(&buf, |x| x.add(3));
-            let gpu_or_cpu = device.modules.gpu_or_cpu.borrow();
-            let (_, operations) = gpu_or_cpu.iter().next().unwrap();
-            assert_eq!(operations.len(), 2);
-            let analyzations = operations.iter().cloned().collect::<Vec<Analyzation>>();
-            assert_eq!(&analyzations[0].input_lengths, &[6]);
-        }
-
-        let mut json = vec![0,];
-        let mut serializer = serde_json::Serializer::new(&mut json);
-        device.modules.serialize(&mut serializer).unwrap();
-        println!("json: {json:?}");
-        // device.modules.save_as_json(".")
-    }
-
 }
---
+++ src/modules/fork/serde.rs
@@ -0,0 +1,87 @@
+use core::cell::RefCell;
+use std::collections::HashMap;
+use super::Fork;
+
+#[cfg(feature = "serde")]
+impl<Mods> serde::Serialize for Fork<Mods> {
+    #[inline]
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer 
+    {
+        self.gpu_or_cpu.borrow().serialize(serializer)
+    }
+}
+
+#[cfg(all(feature = "serde", not(feature = "no-std")))]
+impl<Mods> Fork<Mods> {
+    #[inline]
+    pub fn load_from_deserializer<D: serde::Deserializer<'static>>(&mut self, deserializer: D) -> Result<(), D::Error> {
+        use serde::Deserialize;
+
+        self.gpu_or_cpu = RefCell::new(HashMap::deserialize(deserializer)?);
+        Ok(())
+    }
+
+    #[cfg(feature = "json")]
+    #[inline]
+    pub fn save_as_json(&self, path: impl AsRef<std::path::Path>) -> crate::Result<()> {
+        serde_json::to_writer(std::fs::File::open(path)?, self)?;
+        Ok(())
+    }
+
+    #[cfg(feature = "json")]
+    #[inline]
+    pub fn load_from_json_read(&mut self, reader: impl std::io::Read) -> serde_json::Result<()> {
+        self.load_from_deserializer(&mut serde_json::Deserializer::from_reader(reader))
+    }
+    
+    #[cfg(feature = "json")]
+    #[inline]
+    pub fn load_from_json(&mut self, path: impl AsRef<std::path::Path>) -> crate::Result<()> {
+        self.load_from_json_read(std::fs::File::open(path)?)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[cfg(feature = "json")]
+    #[cfg(feature = "serde")]
+    #[cfg(feature = "opencl")]
+    #[test]
+    fn test_fork_deserialize() {
+        use std::collections::HashMap;
+
+        use serde::Serialize;
+
+        use crate::{OpenCL, Fork, Cached, Base, ApplyFunction, Device, Combiner};
+
+        let device = OpenCL::<Fork<Cached<Base>>>::new(0).unwrap();
+        if !device.unified_mem() {
+            return;
+        }
+        let buf = device.buffer([1, 2, 4, 5, 6, 7]);
+        let out = device.apply_fn(&buf, |x| x.add(3));
+        assert_eq!(out.read(), [4, 5, 7, 8, 9, 10]);
+
+        for _ in 0..100 {
+            let _out = device.apply_fn(&buf, |x| x.add(3));
+            let gpu_or_cpu = device.modules.gpu_or_cpu.borrow();
+            let (_, operations) = gpu_or_cpu.iter().next().unwrap();
+            assert_eq!(operations.len(), 2);
+            let analyzations = operations.iter().cloned().collect::<Vec<_>>();
+            assert_eq!(&analyzations[0].input_lengths, &[6]);
+        }
+
+        let mut json = vec![0,];
+        let mut serializer = serde_json::Serializer::new(&mut json);
+
+        let map = HashMap::from([((32, 32), 53)]);
+        map.serialize(&mut serializer).unwrap();
+        println!("json: {json:?}");
+        // device.modules.save_as_json(".")
+    }
+
+   
+}
