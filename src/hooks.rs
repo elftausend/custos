5a585739d02ac86f53e287aa08cb3d74e9397925
elftausend <76885970+elftausend@users.noreply.github.com>
2024-01-12 10:18:43 +0100

    default () shape for on new buffer

--- src/hooks.rs
+++ src/hooks.rs
@@ -6,7 +6,7 @@ pub trait OnDropBuffer: WrappedData {
     fn on_drop_buffer<T, D: Device, S: Shape>(&self, _device: &D, _buf: &Buffer<T, D, S>) {}
 }
 
-pub trait OnNewBuffer<T, D: Device, S: Shape> {
+pub trait OnNewBuffer<T, D: Device, S: Shape = ()> {
     #[track_caller]
     fn on_new_buffer(&self, _device: &D, _new_buf: &Buffer<T, D, S>) {}
 }
