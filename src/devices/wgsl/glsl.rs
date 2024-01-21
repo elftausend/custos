e68a3eff50c00ff4d486b6a6530f9be38b49e178
elftausend <76885970+elftausend@users.noreply.github.com>
2024-01-21 20:02:11 +0100

    implement from_wgsl function for Glsl

--- src/devices/wgsl/glsl.rs
+++ src/devices/wgsl/glsl.rs
@@ -4,12 +4,19 @@ use naga::{
     valid::ModuleInfo,
 };
 
-use super::error::TranslateError;
+use super::{parse_and_output, error::TranslateError};
 
-pub struct Glsl {}
+#[derive(Debug, Clone)]
+pub struct Glsl {
+    pub sources: Vec<String>
+}
 
 impl Glsl {
-    pub fn from_wgsl(src: impl AsRef<str>) {}
+    pub fn from_wgsl(src: impl AsRef<str>) -> Result<Self, TranslateError> {
+        Ok(Glsl {
+            sources: parse_and_output(src, write_glsl)?,
+        })
+    }
 }
 
 pub fn write_glsl(
@@ -41,7 +48,7 @@ pub fn write_glsl(
 
 #[cfg(test)]
 mod tests {
-    use super::write_glsl;
+    use super::Glsl;
 
     #[test]
     fn test_wgsl_to_glsl_translation() {
@@ -51,5 +58,9 @@ mod tests {
                 return vec4<f32>(1.0, 0.0, 0.0, 1.0);
             }
         ";
+        let glsl = Glsl::from_wgsl(wgsl).unwrap();
+        let is = format!("{glsl:?}");
+        let should = r##"Glsl { sources: ["#version 310 es\n\nprecision highp float;\nprecision highp int;\n\nlayout(location = 0) out vec4 _fs2p_location0;\n\nvoid main() {\n    _fs2p_location0 = vec4(1.0, 0.0, 0.0, 1.0);\n    return;\n}\n\n"] }"##;
+        assert_eq!(is, should);
     }
 }
