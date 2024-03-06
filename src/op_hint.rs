18e42b91ccff2a842daf56818863de8638a78f33
elftausend <76885970+elftausend@users.noreply.github.com>
2024-03-06 14:41:36 +0100

    Add unary chain fure performance test

--- src/op_hint.rs
+++ src/op_hint.rs
@@ -99,4 +99,58 @@ mod tests {
             assert_eq!(*out, buf.sin().cos().ln());
         }
     }
+
+    #[cfg(feature = "cpu")]
+    #[cfg(feature = "lazy")]
+    #[ignore = "too long runtime"]
+    #[test]
+    fn test_op_hint_unary_chain_fuse_perf() {
+        use std::time::Instant;
+
+        use crate::{ApplyFunction, Base, Combiner, Device, Lazy, CPU};
+
+        let dev = CPU::<Lazy<Base>>::new();
+
+        const N: usize = 100000000;
+
+        let buf = dev.buffer::<_, (), _>(vec![1.; N]);
+        let out = dev.apply_fn(&buf, |x| x.sin());
+        let out = dev.apply_fn(&out, |x| x.cos());
+        let _out = dev.apply_fn(&out, |x| x.ln());
+
+        let mut out = buf.clone();
+
+        let start = Instant::now();
+
+        for out in out.iter_mut() {
+            for op in &dev.modules.graph.borrow().operations {
+                let resolve = Resolve {
+                    val: *out,
+                    marker: "x",
+                };
+                if let OpHint::Unary(op) = &op.op_hint {
+                    *out = op(resolve).eval();
+                } else {
+                    panic!()
+                }
+            }
+        }
+
+        println!("cpu automatic fusing: {:?}", start.elapsed());
+        
+        let mut should = buf.clone();
+
+        let start = Instant::now();
+
+        for should in should.iter_mut() {
+            *should = should.sin().cos().ln();
+        }
+
+        println!("cpu manual fusing: {:?}", start.elapsed());
+        
+
+        for (should, out) in should.iter().zip(out.iter()) {
+            assert_eq!(out, should);
+        }
+    }
 }
