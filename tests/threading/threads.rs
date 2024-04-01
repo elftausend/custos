eb08cfdc01dfa13d8a23cd34ec0b4868ce0de0a6
elftausend <76885970+elftausend@users.noreply.github.com>
2024-04-01 22:44:10 +0200

    Add comp time threading test

--- tests/threading/threads.rs
+++ tests/threading/threads.rs
@@ -22,3 +22,26 @@ fn test_with_threads() {
     });
     a.join().unwrap();
 }
+
+#[cfg(feature = "cpu")]
+#[ignore = "compile time"]
+#[cfg_attr(miri, ignore)]
+#[test]
+fn test_with_threads_static() {
+    use custos::Base;
+
+    // use this device instead: compilation error! (as expected)
+    // let device = &*Box::leak(Box::new(CPU::<custos::Cached<Base>>::new()));
+    let device = &*Box::leak(Box::new(CPU::<Base>::new()));
+
+    let buf = Buffer::<f32, _>::new(device, 10);
+
+    let a = std::thread::spawn(move || {
+        let _b = buf;
+
+        let _buf = Buffer::from((device, [1, 2, 3, 4]));
+    });
+    a.join().unwrap();
+}
+
+
