57db83e5339d19a6bbfe85f4d29d498828c7fd0d
elftausend <76885970+elftausend@users.noreply.github.com>
2024-10-06 00:15:43 +0200

    Add MaybeData (#65)

--- src/modules/lazy/wrapper.rs
+++ src/modules/lazy/wrapper.rs
@@ -1,14 +1,16 @@
+mod maybe_data;
+pub use maybe_data::MaybeData;
+
 use core::{
     marker::PhantomData,
     ops::{Deref, DerefMut},
 };
 
-use crate::{flag::AllocFlag, HasId, HostPtr, Id, Lazy, PtrType, ShallowCopy, WrappedData};
+use crate::{flag::AllocFlag, HasId, HostPtr, Lazy, PtrType, ShallowCopy, WrappedData};
 
 #[derive(Debug, Default)]
 pub struct LazyWrapper<Data, T> {
-    pub data: Option<Data>,
-    pub id: Option<Id>,
+    pub maybe_data: MaybeData<Data>,
     pub _pd: PhantomData<T>,
 }
 
@@ -18,29 +20,29 @@ impl<T2, Mods: WrappedData> WrappedData for Lazy<'_, Mods, T2> {
     #[inline]
     fn wrap_in_base<T, Base: HasId + PtrType>(&self, base: Base) -> Self::Wrap<T, Base> {
         LazyWrapper {
-            data: Some(self.modules.wrap_in_base(base)),
-            id: None,
+            maybe_data: MaybeData::Data(self.modules.wrap_in_base(base)),
             _pd: PhantomData,
         }
     }
 
     #[inline]
     fn wrapped_as_base<T, Base: HasId + PtrType>(wrap: &Self::Wrap<T, Base>) -> &Base {
-        Mods::wrapped_as_base(wrap.data.as_ref().expect(MISSING_DATA))
+        Mods::wrapped_as_base(wrap.maybe_data.data().expect(MISSING_DATA))
     }
 
     #[inline]
     fn wrapped_as_base_mut<T, Base: HasId + PtrType>(wrap: &mut Self::Wrap<T, Base>) -> &mut Base {
-        Mods::wrapped_as_base_mut(wrap.data.as_mut().expect(MISSING_DATA))
+        Mods::wrapped_as_base_mut(wrap.maybe_data.data_mut().expect(MISSING_DATA))
     }
 }
 
 impl<Data: HasId, T> HasId for LazyWrapper<Data, T> {
     #[inline]
     fn id(&self) -> crate::Id {
-        match self.id {
-            Some(id) => id,
-            None => self.data.as_ref().unwrap().id(),
+        match self.maybe_data {
+            MaybeData::Data(ref data) => data.id(),
+            MaybeData::Id(id) => id,
+            MaybeData::None => unimplemented!()
         }
     }
 }
@@ -48,23 +50,23 @@ impl<Data: HasId, T> HasId for LazyWrapper<Data, T> {
 impl<Data: PtrType, T> PtrType for LazyWrapper<Data, T> {
     #[inline]
     fn size(&self) -> usize {
-        match self.id {
-            Some(id) => id.len,
-            None => self.data.as_ref().unwrap().size(),
+        match self.maybe_data {
+            MaybeData::Data(ref data) => data.size(),
+            MaybeData::Id(id) => id.len,
+            MaybeData::None => unimplemented!()
         }
     }
 
     #[inline]
     fn flag(&self) -> AllocFlag {
-        self.data
-            .as_ref()
+        self.maybe_data.data()
             .map(|data| data.flag())
             .unwrap_or(AllocFlag::Lazy)
     }
 
     #[inline]
     unsafe fn set_flag(&mut self, flag: AllocFlag) {
-        self.data.as_mut().unwrap().set_flag(flag)
+        self.maybe_data.data_mut().unwrap().set_flag(flag)
     }
 }
 
@@ -76,26 +78,26 @@ impl<Data: Deref<Target = [T]>, T> Deref for LazyWrapper<Data, T> {
 
     #[inline]
     fn deref(&self) -> &Self::Target {
-        self.data.as_ref().expect(MISSING_DATA)
+        self.maybe_data.data().expect(MISSING_DATA)
     }
 }
 
 impl<Data: DerefMut<Target = [T]>, T> DerefMut for LazyWrapper<Data, T> {
     #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
-        self.data.as_mut().expect(MISSING_DATA)
+        self.maybe_data.data_mut().expect(MISSING_DATA)
     }
 }
 
 impl<T, Data: HostPtr<T>> HostPtr<T> for LazyWrapper<Data, T> {
     #[inline]
     fn ptr(&self) -> *const T {
-        self.data.as_ref().unwrap().ptr()
+        self.maybe_data.data().unwrap().ptr()
     }
 
     #[inline]
     fn ptr_mut(&mut self) -> *mut T {
-        self.data.as_mut().unwrap().ptr_mut()
+        self.maybe_data.data_mut().unwrap().ptr_mut()
     }
 }
 
@@ -103,8 +105,11 @@ impl<Data: ShallowCopy, T> ShallowCopy for LazyWrapper<Data, T> {
     #[inline]
     unsafe fn shallow(&self) -> Self {
         LazyWrapper {
-            id: self.id,
-            data: self.data.as_ref().map(|data| data.shallow()),
+            maybe_data: match &self.maybe_data {
+                MaybeData::Data(data) => MaybeData::Data(data.shallow()),
+                MaybeData::Id(id) => MaybeData::Id(*id),
+                MaybeData::None => unimplemented!(),
+            },
             _pd: PhantomData,
         }
     }
