dde52ce9a9868ff637a16cb53f7cf5086c201b52
elftausend <76885970+elftausend@users.noreply.github.com>
2024-10-13 14:06:29 +0200

    Add get_ref and get_mut to CowMut

--- src/cow_mut.rs
+++ src/cow_mut.rs
@@ -1,7 +1,4 @@
-use core::{
-    default,
-    ops::{Deref, DerefMut},
-};
+use core::ops::{Deref, DerefMut};
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize))]
 #[derive(Debug)]
@@ -17,6 +14,25 @@ impl<T: Default> Default for CowMut<'_, T> {
 }
 
 impl<'a, T> CowMut<'a, T> {
+    #[inline]
+    pub fn get_ref(&self) -> &T {
+        match self {
+            // CowMut::Borrowed(borrowed) => borrowed,
+            CowMut::BorrowedMut(borrowed_mut) => borrowed_mut,
+            CowMut::Owned(owned) => owned,
+        }
+    }
+
+    #[inline]
+    pub fn get_mut(&mut self) -> Option<&mut T> {
+        match self {
+            // CowMut::Borrowed(_borrowed) => None,
+            CowMut::BorrowedMut(borrowed_mut) => Some(borrowed_mut),
+            CowMut::Owned(owned) => Some(owned),
+        }
+    }
+
+    #[inline]
     pub fn is_owned(&self) -> bool {
         match self {
             CowMut::BorrowedMut(_) => false,
@@ -24,6 +40,7 @@ impl<'a, T> CowMut<'a, T> {
         }
     }
 
+    #[inline]
     pub fn into_owned(self) -> T 
     where 
         T: Clone
