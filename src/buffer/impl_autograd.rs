741c6fe74851d0b81e2c304c0c06655e2c7bc603
elftausend <76885970+elftausend@users.noreply.github.com>
2025-05-17 17:38:10 +0200

    Add grad_mut_self dummy method to Buffer when autograd feature is disabled

--- src/buffer/impl_autograd.rs
+++ src/buffer/impl_autograd.rs
@@ -124,6 +124,15 @@ where
         (self, unsafe { self.grad_mut_unbound() })
     }
 
+    #[cfg(not(feature = "autograd"))]
+    #[inline]
+    pub fn grad_mut_self<'b: 'c, 'c>(&'b mut self) -> (&'c Self, &'b mut Buffer<'static, T, D, S>)
+    where
+        D: GradActions + Alloc<T> + ZeroGrad<T>,
+    {
+        unimplemented!("Gradient not available. Activate the autograd feature.");
+    }
+
     #[cfg(feature = "autograd")]
     #[inline]
     pub fn grad_mut<'b>(&'b mut self) -> &'b mut Buffer<'static, T, D, S>
